eda8296abea64c2c48ee8a95e8a11a8965e8b096
vesper2108 <30739917+Chintan2108@users.noreply.github.com>
2018-10-21 18:22:45 +0530

    initial commit

---
+++ trial/userlogin/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+
+def user_login(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/accounts/')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render_to_response('accounts/profile.html', {}, context)
