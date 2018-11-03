046ae5b82d5542ca2b48b3b5fcd3007669de9226
Chintan2108 <chintanmaniyar@gmail.com>
2018-11-03 20:00:31 +0530

    final commit

---
+++ Firsts/catalog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def index(request):
+    """View function for home page of site."""
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
+    context = {
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
